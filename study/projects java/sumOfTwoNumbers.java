6c8b199f46edd6ab8cb74b2834a5cd25c505bac7
Léo Taiki Tamura <tamuraleo2005@gmail.com>
2024-02-07 15:47:55 -0300

    commit

--- study/projects java/sumOfTwoNumbers.java
+++ study/projects java/sumOfTwoNumbers.java
@@ -1,8 +1,8 @@
 public class sumOfTwoNumbers {
     public static int sum(int a, int b) 
 	{
-        a = 3;
-		b = 2;
+        int a = 3;
+		int b = 2;
 		System.err.println(Integer.sum(a, b));
     }
 }
