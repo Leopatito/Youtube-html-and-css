5bbfd03961d219eff042d475973b016e14a48eb9
Léo Taiki Tamura <tamuraleo2005@gmail.com>
2024-03-02 16:28:37 -0300

    adjustments

--- study/projects java/sumOfTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-public class sumOfTwoNumbers {
-    public static int sum(int a, int b) 
-	{
-        int a = 3;
-		int b = 2;
-		System.err.println(Integer.sum(a, b));
-    }
-}
--- study/projects/madlibs.py
+++
@@ -1,8 +0,0 @@
-adj = input("digite um adjetivo no plural no feminino:")
-noun = input("digite um substantivo no plural:")
-adverb = input("digite um advérbio:")
-print("rosas são "+ str(adj))
-print(str(noun)+ " são violetas")
-print("e o mundo está fazendo "+ str(adverb))
-
-
